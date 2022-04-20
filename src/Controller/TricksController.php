e2cd25f7d7d4844610dcd3c1c2ee0f307ec4d975
Christophe <christophe.dumas1@gmail.com>
2022-04-20 21:58:51 +0200

    📦 NEW: newTricks controller

--- src/Controller/TricksController.php
+++ src/Controller/TricksController.php
@@ -2,10 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Media;
+use App\Entity\Tricks;
+use App\Form\CreateTricksType;
 use App\Repository\TricksRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use DateTime;
+use DateTimeImmutable;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TricksController extends AbstractController
 {
@@ -28,6 +34,40 @@ class TricksController extends AbstractController
         $allTricks = $repository->findAll();
         return $this->render('tricks/allTricks.html.twig', [
             'allTricks' => $allTricks
+        ]);
+    }
+
+    /**
+     * @Route("/newTricks", name="newTricks")
+     */
+    public function newTricks(Request $request)
+    {
+        $newTricks = new Tricks();
+        $form = $this->createForm(CreateTricksType::class, $newTricks);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $images = $form->get('medias')->getData();
+            foreach($images as $image){
+                $fichier = md5(uniqid()).'.'.$image->guessExtension();
+                $image->move(
+                    $this->getParameter('images_directory'),
+                    $fichier
+                );
+                $img = new Media();
+                $img->setName($fichier);
+                $newTricks->addMedia($img);
+            }
+            $newTricks->setCreatedAt(new DateTimeImmutable());
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($newTricks);
+            $em->flush();
+
+            $this->addFlash('success', 'Votre nouveau tricks est crée !!');
+            return $this->redirectToRoute('home');
+        }
+        return $this->render('tricks/newTricks.html.twig', [
+            'formNewTricks' => $form->createView()
         ]);
     }
 
